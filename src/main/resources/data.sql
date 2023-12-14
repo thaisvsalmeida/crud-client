5b3027e754387448cb06c71765977cd1f01b5cde
Thaís Almeida <thaisvsalmeida@gmail.com>
2023-12-14 10:58:53 -0300

    CRUD clients DevSuperior Bootcamp

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Lucas', 12312312312, 5200.50, TIMESTAMP WITH TIME ZONE '1990-07-13T20:50:07.12345Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Joana', 45645645645, 10000.00, TIMESTAMP WITH TIME ZONE '1995-06-15T10:52:07.12345Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Marcela', 78978978978, 3000.20, TIMESTAMP WITH TIME ZONE '1989-05-20T15:54:07.12345Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Bruno', 74174174174, 6515.60, TIMESTAMP WITH TIME ZONE '1985-04-02T12:12:07.12345Z', 3);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Ariel', 85285285285, 6429.40, TIMESTAMP WITH TIME ZONE '2000-03-25T13:13:07.12345Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Italo', 96396396396, 8745.20, TIMESTAMP WITH TIME ZONE '2001-02-14T22:14:07.12345Z', 4);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Luciana', 14714714714, 25324.30, TIMESTAMP WITH TIME ZONE '1996-01-04T21:50:07.12345Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Maria', 25825825825, 12354.00, TIMESTAMP WITH TIME ZONE '1999-12-16T17:55:07.12345Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Walter', 36936936936, 6359.40, TIMESTAMP WITH TIME ZONE '2002-11-18T03:20:07.12345Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Talia', 75375375375, 8456.80, TIMESTAMP WITH TIME ZONE '1994-10-22T05:30:07.12345Z', 2);
